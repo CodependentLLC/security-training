1f7f77ce51efa73a8ce12a213b68fd5421c86b04
Phil Knowles <fighters4christ@outlook.com>
2025-11-16 16:17:04 -0600

    Build for GitHub Pages from dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/security-training/",   // 👈 repo name, between slashes
-  build: {
-    outDir: "dist",              // 👈 we'll deploy from docs/ to avoid confusion
-  },
+  base: "/security-training/", // 👈 repo name between slashes
 });
